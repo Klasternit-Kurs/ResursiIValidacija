108a1e417cb306a8217887ef8f26cb010d9d70cc
Dragan Pepic <d.pepic@hotmail.com>
2020-04-03 19:54:48 +0200

    Promene

--- ResursiIValidacija/ValidatorZaGodine.cs
+++ ResursiIValidacija/ValidatorZaGodine.cs
@@ -12,13 +12,14 @@ namespace ResursiIValidacija
 	{
 		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
 		{
-			if (int.TryParse(value.ToString(), out _))
+			if (int.TryParse(value.ToString(), out int broj))
 			{
-				return ValidationResult.ValidResult;
+				if (broj >= 18 && broj <= 100)
+					return ValidationResult.ValidResult;
+				else
+					return new ValidationResult(false, "Broj nije izmedju 18 i 100");
 			} else
-			{
 				return new ValidationResult(false, "Ovo nije broj :/");
-			}
 		}
 	}
 }
