99e9c89b5d24c743e71bed06b6f7a01672a1d46e
Dragan Pepic <d.pepic@hotmail.com>
2020-04-03 19:06:10 +0200

    Add project files.

---
+++ ResursiIValidacija/Osoba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResursiIValidacija
+{
+	public class Osoba : INotifyPropertyChanged
+	{
+		private string ime;
+		public string Ime 
+		{ 
+			get => ime;
+
+			set
+			{
+				ime = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Ime"));
+			}
+		}
+
+		private int godine = 20;
+		public int Godine
+		{
+			get => godine;
+
+			set
+			{
+				godine = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Godine"));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public override string ToString() => Ime;
+		public Osoba(string i)
+		{
+			Ime = i;
+		}
+		public Osoba() { }
+	}
+}
---
+++ ResursiIValidacija/ValidatorZaGodine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ResursiIValidacija
+{
+	class ValidatorZaGodine : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			if (int.TryParse(value.ToString(), out _))
+			{
+				return ValidationResult.ValidResult;
+			} else
+			{
+				return new ValidationResult(false, "Ovo nije broj :/");
+			}
+		}
+	}
+}
